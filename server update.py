04d07485f33ea68ad59bb7b1324dc78d2dd8dbd6
Duedot43 <75505449+Duedot43@users.noreply.github.com>
2021-01-08 17:20:39 -0700

    Add files via upload

---
+++ server update.py
@@ -0,0 +1,75 @@
+#!/bin/bash
+import os
+cwd = os.getcwd()
+#import sys
+import webbrowser
+import subprocess
+from tkinter import filedialog
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import *
+master = Tk()
+var = IntVar()
+var.set(1)
+
+def quit_loop():
+    print("Selection:",var.get())
+    global selection
+    selection = var.get()
+    master.quit()
+
+Label(master, text = "Select an Option").grid(row=0, sticky=W)
+Radiobutton(master, text = "Reset world", variable=var, value = 1).grid(row=1, sticky=W)
+Radiobutton(master, text = "Reset nether", variable=var, value = 2).grid(row=2, sticky=W)
+Radiobutton(master, text = "Configure server.prop", variable=var, value = 3).grid(row=3, sticky=W)
+Radiobutton(master, text = "Add a plugin", variable=var, value = 4).grid(row=4, sticky=W)
+Radiobutton(master, text = "Add a world file", variable=var, value = 5).grid(row=5, sticky=W)
+#backup world
+Radiobutton(master, text = "Start Server", variable=var, value = 6).grid(row=6, sticky=W)
+Button(master, text = "GO!", command=quit_loop).grid(row=7, sticky=W)
+
+master.mainloop()
+
+if selection == 1:
+    os.system("cd server ; rm -rf world")
+if selection == 2:
+    os.system("cd server ; rm -rf world_nether")
+if selection == 3:
+    os.system("cd server ; open server.properties /System/Applications/TextEdit.app")
+if selection == 4:
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    subprocess.call(["cp", file_path, "server/plugins"])
+if selection == 5:
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    subprocess.call(["rm", "-rf", "world", ";", "cp", file_path, "server"])
+        
+MsgBox = tk.messagebox.askquestion ('Use Ngrok?','Do you want to use ngrok or be guided to server.properties to configure your server?',icon = 'warning')
+if MsgBox == 'yes':
+    cwd = os.getcwd()
+    #cwd2 = cwd"/start.command"
+    subprocess.call(["open", cwd, "-n", "-a", "/System/Applications/Utilities/Terminal.app"])# ";", "./ngrok", "tcp", "25565"])
+if MsgBox == 'no':
+    os.system("cd server ; open server.properties /System/Applications/TextEdit.app") 
+os.system("git clone https://github.com/Duedot43/update.git ; cd update ; cp update.txt ..")
+updates = ()
+#git hub crap i will add later
+update = open('update.txt','r')
+for line in update:
+    updates = line.strip()
+os.system("rm -rf update ; rm update.txt")
+if updates == '1':
+    messagebox.showinfo(title='Update!', message='there is an update')
+    os.system('cd server ; git clone https://github.com/Duedot43/minecraft-update.git ; rm papper.jar ; cd minecraft-update ; cp papper.jar .. ; cd .. ; rm -rf minecraft-update')
+box = messagebox.askquestion(title='Start Server?', message='Start The Server?')
+if box == 'yes':
+    #subprocess.call(["cd", "server", ";", "./start_server.command"])
+    os.system("cd server ; java -Xmx1024M -Xms1024M -jar papper.jar")
+    exit()
+if box == 'no':
+    exit()
